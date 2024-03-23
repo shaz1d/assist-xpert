6cf1c1f8b5d00d4cc3395b57ac893d756741b5e8
Shazid <shazid.dev@gmail.com>
2024-03-23 11:25:14 +0600

    fix: font weight and letter spacing of heading

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -13,10 +13,10 @@ const Hero = () => {
       ></Image>
       <div className="container-x grid grid-cols-2 md:grid-cols-3 relative z-20">
         <div className="flex flex-col gap-4 items-start col-span-2">
-          <h1 className="text-6xl font-bold">
+          <h1 className="text-6xl leading-tight">
             Navigate Growth with Expert/Smart Guidance
           </h1>
-          <p className="max-w-3xl text-2xl">
+          <p className="max-w-4xl text-2xl uppercase tracking-widest">
             Streamlined Processes, Expert Assistance, and Powerful Web Solutions
             for Business Growth.
           </p>
